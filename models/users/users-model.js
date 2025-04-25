cbbc09f6397d4bff98bc5fa98eacb5de9602b495
Jeevitha <jeevitha.pugazh@gmail.com>
2025-04-25 17:11:23 -0400

    🦺 Fix(User)-Validation
    Implemented user data validation

--- models/users/users-model.js
+++ models/users/users-model.js
@@ -4,17 +4,35 @@ const usersSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
+      required: [true, "Username is required"],
       lowercase: true,
+      index: true,
+      minlength: [
+        3,
+        "Username must be at least 3 characters long",
+      ],
+      maxlength: [
+        30,
+        "Username must be at most 30 characters long",
+      ],
     },
     email: {
       type: String,
-      unique: [true, "Email must be unique"],
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /.+@.+\..+/,
+        "Please enter a valid email address",
+      ],
     },
     password: {
       type: String,
-      required: true,
-      minlength: 8,
+      required: [true, "Password is required"],
+      minlength: [
+        8,
+        "Password must be at least 8 characters long",
+      ],
     },
   },
   { timestamps: true }
